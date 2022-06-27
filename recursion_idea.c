08c23380191149122799eed3e6bf89d08b2ec27c
mehedi hasan <mehedicsebu@gmail.com>
2022-06-28 00:09:30 +0600

    recursion idead added

---
+++ recursion_idea.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void number(int n)
+{
+    if (n == 0)
+        return;
+    number(n - 1);
+    printf("%d\n", n);
+}
+
+int main()
+{
+    number(10);
+    printf("ENd");
+    return 0;
+}
