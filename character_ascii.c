697d3e32f7e35e6513ee7e20034e4c9ea2405945
mehedi hasan <mehedicsebu@gmail.com>
2022-07-06 00:14:17 +0600

    character and ASCII

---
+++ character_ascii.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+
+    char a = 'M', b = 'U', c = 'k';
+    printf("%c%c%c\n", a, b, c);
+    // Asci valu
+    char m = 'A';
+    int n = 65;
+    printf("%c\n", m);
+    printf("%d\n", m);
+    printf("%c\n", n);
+    return 0;
+}
