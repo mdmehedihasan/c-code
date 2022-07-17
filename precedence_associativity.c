5c5da158cb0b06514bc4cbf42a7c0884536f25a2
mehedi hasan <mehedicsebu@gmail.com>
2022-07-17 20:08:27 +0600

    precedence and associativity

---
+++ precedence_associativity.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    // precedence
+    //  if (5 > 10 - 5 && 5)
+    // associativity
+    if (1 == 1 != 0)
+    {
+        printf("HI\n");
+    }
+    else
+        printf("Hello");
+    return 0;
+}
