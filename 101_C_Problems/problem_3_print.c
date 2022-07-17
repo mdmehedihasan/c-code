3f3139d894b75c894ac41350983d56e2a2d7757d
mehedi hasan <mehedicsebu@gmail.com>
2022-07-18 02:01:58 +0600

    problem 3

---
+++ 101_C_Problems/problem_3_print.c
@@ -0,0 +1,11 @@
+/*write a code to print the following out put
+Name: Mr.X
+Univ: Bangladesh University
+*/
+
+#include <stdio.h>
+int main()
+{
+    printf("Name:Mr.X\nUniv: Bangladesh University");
+    return 0;
+}
