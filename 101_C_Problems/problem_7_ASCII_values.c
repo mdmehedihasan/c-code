54baf973005e9551fc91ae92eb88f36ee44defdf
mehedi hasan <mehedicsebu@gmail.com>
2022-07-18 18:16:03 +0600

    problem 7

---
+++ 101_C_Problems/problem_7_ASCII_values.c
@@ -0,0 +1,13 @@
+/*Write a program to print the following output
+ASCII value of A is:__
+ASCII value fo @ is:__
+
+In the blank space you should show the ASCII code of the given characters.
+*/
+#include <stdio.h>
+int main()
+{
+    printf("ASCII value of A is: %d\n", 'A');
+    printf("ASCII value of @ is : %d", '@');
+    return 0;
+}
