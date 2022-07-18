3c19a4bb0286e1d39931f346a901984fa436763b
mehedi hasan <mehedicsebu@gmail.com>
2022-07-18 18:31:28 +0600

    problem 9

---
+++ 101_C_Problems/problem_9_Bangladesh.c
@@ -0,0 +1,10 @@
+/*Print "Bangladesh" without quotation sign by using ascii vlaues
+use given ascii values:
+B=66, a=97, n=110,g=103,l=108,a=97,d=100,e=101,s=115,h=104
+*/
+#include <stdio.h>
+int main()
+{
+    printf("%c%c%c%c%c%c%c%c%c%c", 66, 97, 110, 103, 108, 97, 100, 101, 115, 104);
+    return 0;
+}
