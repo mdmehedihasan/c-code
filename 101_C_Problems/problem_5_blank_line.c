2e2151aa83d228f4d69c73332cbcc1577b35d039
mehedi hasan <mehedicsebu@gmail.com>
2022-07-18 17:57:33 +0600

    problem 5

---
+++ 101_C_Problems/problem_5_blank_line.c
@@ -0,0 +1,7 @@
+// write a program to print the following two blank lines in the middle
+#include <stdio.h>
+int main()
+{
+    printf("Name:\tMr.DON\nCountry:\tBangladesh \n\n\ncontact:mr.don@mycompany.com");
+    return 0;
+}
