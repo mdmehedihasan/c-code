ef7d31b52bfbbe79dcd093e4c72c7655c0781b58
mehedi hasan <mehedicsebu@gmail.com>
2022-07-18 17:24:13 +0600

    problemn 4

---
+++ 101_C_Problems/problem_4_diamond.c
@@ -0,0 +1,17 @@
+// Write a program to print diamond output
+#include <stdio.h>
+int main()
+{
+    printf("    *\n   ***\n  *****\n ******* \n*********\n *******\n  *****\n   ***\n    *");
+    printf("\n\n");
+    printf("    *\n");
+    printf("   ***\n");
+    printf("  *****\n");
+    printf(" *******\n");
+    printf("*********\n");
+    printf(" *******\n");
+    printf("  *****\n");
+    printf("   ***\n");
+    printf("    *\n");
+    return 0;
+}
