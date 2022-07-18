f70c15af376f8e85d1fbbf20b5fffa50cf897276
mehedi hasan <mehedicsebu@gmail.com>
2022-07-18 18:02:46 +0600

    problem 6

---
+++ 101_C_Problems/problem_6_variable.c
@@ -0,0 +1,9 @@
+/*take three integer variable a,b and c. Assign values to the variables a=10,b=20 and c=30, Now print the sum of these three variables*/
+
+#include <stdio.h>
+int main()
+{
+    int a = 10, b = 20, c = 30;
+    printf("Sum of value is: %d", a + b + c);
+    return 0;
+}
