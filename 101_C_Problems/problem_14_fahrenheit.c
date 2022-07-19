14d53d347b4aa2c75291e35f92ee0593f6989e62
mehedi hasan <mehedicsebu@gmail.com>
2022-07-19 13:03:21 +0600

    problem 14

--- 101_C_Problems/problem_14_fahrenheit.c
+++ 101_C_Problems/problem_14_fahrenheit.c
@@ -2,3 +2,13 @@
 Take temporary of Dhaka City as input in celcius scale from the user scanf() and convert it to Fahrenheit.
 formula: F=c(9/5)+32
 */
+#include <stdio.h>
+int main()
+{
+
+    float celcius;
+    scanf("%f", &celcius);
+    printf("\nFahrenheit is: %.3f", celcius * (9.0 / 5.0) + 32);
+
+    return 0;
+}
