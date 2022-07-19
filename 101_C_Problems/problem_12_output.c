6c7ae98a65cc6f12e2572802ac9719910cb51398
mehedi hasan <mehedicsebu@gmail.com>
2022-07-19 10:26:46 +0600

    problem 12

--- 101_C_Problems/problem_12_output.c
+++ 101_C_Problems/problem_12_output.c
@@ -5,18 +5,10 @@ Take two integer variables i=0 and j=0 now write the output of the following pro
 int main()
 {
 
-    // first problem
-    int a = 5;
-    printf("%d %d %d %d", ++a, a++, a, ++a);
-    //  printf("%d %d %d %d", --a, a--, a, --a);
-
-    // second problem
-    // int x = 10;
-    // printf("%d %d %d %d %d %d %d %d", x--, --x, x, ++x, x--, x++, ++x, x);
-
-    // int x = 10;
-    // int y = ++x;
-    // printf("%d %d %d %d %d %d", ++x, ++y, y, y++, x++, ++x);
+    int i = 0;
+    int j = 0;
+    j = i++ + ++i;
+    printf("%d %d", i, j);
 
     return 0;
 }
