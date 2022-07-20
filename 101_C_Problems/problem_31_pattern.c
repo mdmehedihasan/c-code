edf49a37b9d41315d74ec215c1a3b199798b7c58
mehedi hasan <mehedicsebu@gmail.com>
2022-07-20 18:45:40 +0600

    problem 31

---
+++ 101_C_Problems/problem_31_pattern.c
@@ -0,0 +1,24 @@
+/*
+write a program to print the following out put
+*/
+#include <stdio.h>
+int main()
+{
+    int i, j;
+    for (i = 1; i <= 7; i++)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            if (i % 2 == 0)
+            {
+                printf("line break");
+                break;
+            }
+            else
+                printf("*");
+        }
+
+        printf("\n");
+    }
+    return 0;
+}
