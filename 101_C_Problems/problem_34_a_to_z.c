b36535cbbfe429431333bae9c823b3c7e3727bf8
mehedi hasan <mehedicsebu@gmail.com>
2022-07-20 19:45:21 +0600

    problem 34

---
+++ 101_C_Problems/problem_34_a_to_z.c
@@ -0,0 +1,12 @@
+/*
+Write a program to display A to Z using loop
+*/
+#include <stdio.h>
+int main()
+{
+    for (int i = 65; i <= 90; i++)
+    {
+        printf("%c\t", i);
+    }
+    return 0;
+}
