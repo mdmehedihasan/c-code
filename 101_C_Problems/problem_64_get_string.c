7ba65a7c3703b6845a207e1e926d2860e90e3e5b
mehedi hasan <mehedicsebu@gmail.com>
2022-08-11 18:41:30 +0600

    problem 64

---
+++ 101_C_Problems/problem_64_get_string.c
@@ -0,0 +1,15 @@
+/*
+write a program to print the following output
+Enter your name:
+Mr. Mehedi Hasan Rubel
+Hell!! Mr. Mehedi Hasan Rubel
+*/
+int main()
+{
+
+    char name[400];
+    gets(name);
+    printf("Hello!! %s", name);
+
+    return 0;
+}
